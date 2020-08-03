5b898e849b7b98d6840c989ae33e8597620b353a
CristainValencia <68874654+CristainValencia@users.noreply.github.com>
2020-08-03 17:44:36 -0500

    Aplicando la observación a la estructura codificada para dar comentarios descriptivos.

--- conexionbd/Conection.java
+++ conexionbd/Conection.java
@@ -1,38 +1,39 @@
-package conexionbd;
+package conexionbd; // Paquete principal - path: C:\Users\javier1\IdeaProjects\Sistema\src\conexionbd
 
-import java.sql.*;
+import java.sql.*; // Bibloteca SQL
 
-public class Conection {
+public class Conection { // Clase principal de conexion
 
-    private boolean banderabd;
-    public Conection() throws SQLException {
+    private boolean banderabd; // Baandera
+    public Conection() throws SQLException { // Constructor BASE
 
-        try {
+        try { // Intenta canalizar código con falla
 
             connBD();
 
-            banderabd = true;
+            banderabd = true; // Bandera - Entrada
 
         } catch (SQLException e){
-            System.err.println(e.getMessage() + "Error en la conexión BASE");
+            System.err.println(e.getMessage() + "Error en la conexión BASE"); // Mensaje si el código falla
 
-            banderabd = false;
+            banderabd = false; // Bandera - Salida
         }
-
+        //Codición de verificación
         if(banderabd == true){
-
+            // Mensaje - Entrada
             System.out.println("¡La entrada de la base de datos BASE, tuvo exito!");
 
         } else {
+            // Accion de - Salida
             connBD().close();
         }
     }
-
+    // Método para la conexión
     public static Connection connBD() throws SQLException{
-
+        // Objeto SQL
         Connection cnnSQL = DriverManager.getConnection("jdbc:mysql://localhost/bd_BASE"
                 ,"Cristian"
                 ,"12345678910121314151617");
-        return cnnSQL;
+        return cnnSQL; // Retorno 
     }
 }
