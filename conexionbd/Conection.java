8ff8115b185b3edeb00f91a042319944029d80f6
CristainValencia <68874654+CristainValencia@users.noreply.github.com>
2020-08-03 10:45:04 -0500

    Realizando la conexión completa, desde mi localhost - servidor localhost con datos de accesesos.

--- conexionbd/Conection.java
+++ conexionbd/Conection.java
@@ -18,6 +18,9 @@ public class Conection {
 
     public static Connection connBD() throws SQLException{
 
-
+        Connection cnnSQL = DriverManager.getConnection("jdbc:mysql://localhost/bd_BASE"
+                ,"Cristian"
+                ,"12345678910121314151617");
+        return cnnSQL;
     }
 }
