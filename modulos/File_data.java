9ab98c382721e6bb4b2108dc174d29061bfe197a
CristainValencia <68874654+CristainValencia@users.noreply.github.com>
2020-08-04 10:52:37 -0500

    Extensiones por defecto del sistema

--- modulos/File_data.java
+++ modulos/File_data.java
@@ -1,6 +1,7 @@
 package modulos;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.io.File;
 
 
@@ -24,10 +25,12 @@ public class File_data {
 
         JFileChooser file_selector = new JFileChooser();
         file_selector.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-
+        FileNameExtensionFilter fileExtensiónRepositorio = new FileNameExtensionFilter(
+                "*.PNG","PNG", "*.JPG", "JPG");
+        file_selector.setFileFilter(fileExtensiónRepositorio);
     }
 
-    
+
     public void file_ex(){
         
     }
